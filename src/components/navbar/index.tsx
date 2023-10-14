f9eb316cbf46e1616f7c31f056d0566ab8077994
Erdem AY <eirdemmoon@gmail.com>
2023-10-15 02:19:12 +0300

    Update Navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -16,7 +16,6 @@ if (typeof window !== "undefined") {
   lastName = window.localStorage.getItem("lastName");
 }
 
-
 const Navbar: React.FC = () => {
   const [token, setToken] = useState<AuthToken>();
   const [isOpen, setIsOpen] = useState(false);
@@ -38,92 +37,95 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-blue-600 p-4 fixed top-0 left-0 w-full">
-      <div className="mx-auto max-w-7xl w-11/12 flex justify-between items-center">
-        <Link href="/">
-          <div className="flex items-center cursor-pointer space-x-2 text-white hover:text-blue-300 transition">
-            <FaHome className="text-3xl" />
-            <span className="text-2xl font-bold">Wy Blog</span>
-          </div>
-        </Link>
-        <div className="flex space-x-4">
-          <CustomLink
-            href="/about"
-            label="About"
-            icon={<FaInfoCircle className="text-xl" />}
-          />
-          <CustomLink
-            href="/contact"
-            label="Contact"
-            icon={<FaEnvelope className="text-xl" />}
-          />
-          {token?.token ? (
-            <div className="flex space-x-4">
-              <div className="flex my-auto relative">
-                <BsFillPersonFill
-                  onClick={toggleDiv}
-                  className="text-2xl text-green-500 hover:text-white cursor-pointer"
-                />
-                {isOpen && (
-                  <div className="absolute top-8 flex flex-col py-4 mx-auto  right-0 w-44 bg-white p-8 rounded-lg shadow-lg">
-                    <div className="cursor-default flex items-center">
-                      <BsFillPersonFill />
-                      <p className="ml-2">
-                        {firstName} {lastName}
-                      </p>
-                    </div>
-                    <Link href="my-blog">
-                      <div className="flex items-center">
-                        <FaBlog /> <h1 className="ml-2">My Blogs</h1>
-                      </div>
-                    </Link>
-                    <Link href="add-blog">
-                      <div className="flex items-center">
-                        <FaPlus /> <h3 className="ml-2">Add Blog</h3>
-                      </div>
-                    </Link>
-                    <Link href="settings">
-                      <div className="flex items-center">
-                        <FaCog /> <h3 className="ml-2">Settings</h3>
-                      </div>
-                    </Link>
-                    <button
-                      className="text-red-500 cursor-pointer"
-                      onClick={tokenDelete}
-                    >
-                      <div className="flex items-center">
-                        <FaSignOutAlt /> <span className="ml-2">Logout</span>
-                      </div>
-                    </button>
-                  </div>
-                )}
-              </div>
-            </div>
-          ) : (
-            <CustomLink
-              href="/login"
-              label="Login"
-              icon={<BsFillPersonFill className="text-xl" />}
-            />
-          )}
+    <nav className="flex justify-between px-28 items-center bg-white">
+      <Link href="/">
+        <div className="flex items-center cursor-pointer space-x-2 text-black transition">
+        <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="100"
+  height="65"
+  viewBox="0 0 100 100"
+>
+  <circle cx="50" cy="50" r="40" fill="#f39c12" />
+  <text x="50%" y="50%" text-anchor="middle" dy=".0em" font-size="32" fill="#fff">
+    WY
+  </text>
+  <text x="50%" y="65%" text-anchor="middle" dy=".4em" font-size="20" fill="#fff">
+    Blog
+  </text>
+</svg>
         </div>
+      </Link>
+      <div className="flex items-center">
+        <ul className="flex items-center space-x-6">
+          <li className="font-semibold text-gray-700">
+            <Link href="about">
+              <div className="flex items-center">
+                <FaInfoCircle /> <h1 className="ml-2">About</h1>
+              </div>
+            </Link>
+          </li>
+          <li className="font-semibold text-gray-700">
+            <Link href="contact">
+              <div className="flex items-center">
+                <FaEnvelope /> <h1 className="ml-2">Contact</h1>
+              </div>
+            </Link>
+          </li>
+          <li>
+            {token?.token ? (
+              <div className="flex space-x-4">
+                <div className="flex my-auto relative">
+                  <BsFillPersonFill
+                    onClick={toggleDiv}
+                    className="text-2xl text-green-500 hover:text-black cursor-pointer"
+                  />
+                  {isOpen && (
+                    <div className="absolute top-8 flex flex-col py-4 mx-auto  right-0 w-44 bg-white p-8 rounded-lg shadow-lg">
+                      <div className="cursor-default flex items-center">
+                        <BsFillPersonFill />
+                        <p className="ml-2">
+                          {firstName} {lastName}
+                        </p>
+                      </div>
+                      <Link href="my-blog">
+                        <div className="flex items-center">
+                          <FaBlog /> <h1 className="ml-2">My Blogs</h1>
+                        </div>
+                      </Link>
+                      <Link href="add-blog">
+                        <div className="flex items-center">
+                          <FaPlus /> <h3 className="ml-2">Add Blog</h3>
+                        </div>
+                      </Link>
+                      <Link href="settings">
+                        <div className="flex items-center">
+                          <FaCog /> <h3 className="ml-2">Settings</h3>
+                        </div>
+                      </Link>
+                      <button
+                        className="text-red-500 cursor-pointer"
+                        onClick={tokenDelete}
+                      >
+                        <div className="flex items-center">
+                          <FaSignOutAlt /> <span className="ml-2">Logout</span>
+                        </div>
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </div>
+            ) : (
+              <Link href="login">
+                <div className="flex items-center">
+                  <BsFillPersonFill /> <h1 className="ml-2">Login</h1>
+                </div>
+              </Link>
+            )}
+          </li>
+        </ul>
       </div>
     </nav>
-  );
-};
-
-const CustomLink: React.FC<{
-  href: string;
-  label: string;
-  icon: JSX.Element;
-}> = ({ href, label, icon }) => {
-  return (
-    <Link href={href}>
-      <div className="flex items-center cursor-pointer space-x-2 text-white hover:text-blue-300 transition">
-        {icon}
-        <span className="text-lg">{label}</span>
-      </div>
-    </Link>
   );
 };
 
